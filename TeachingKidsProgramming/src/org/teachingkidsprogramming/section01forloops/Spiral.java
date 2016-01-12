3e79b27ef1c46fefe992c72294e8388b2d5f02a0
tsmclain01 <tsmclain01@gmail.com>
2016-01-12 13:11:03 -0600

    Spiral 1
    Made Spiral

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
@@ -1,5 +1,6 @@
 package org.teachingkidsprogramming.section01forloops;
 
+import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
 public class Spiral
@@ -7,21 +8,28 @@ public class Spiral
   public static void main(String[] args)
   {
     //    Show the tortoise --#1
+    Tortoise.show();
     //    Make the tortoise go as fast as possible --#4
+    Tortoise.setSpeed(10);
     //    Add Blue Violet to the Color Wheel --#7
     //    Add Violet to the Color Wheel --#8
     //    Add Purple to the Color Wheel --#9
     //    Do the following 75 times --#3.1
-    try
+    for (int i = 0; i < 75; i++)
     {
-      //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#6
-      //         Move the tortoise 5 times the current line number you are drawing --#5
-      //         Turn the tortoise 1/3 of 360 degrees to the right --#2
+      try
+      {
+        //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#6
+        //         Move the tortoise 5 times the current line number you are drawing --#5
+        Tortoise.move(5 * i);
+        //         Turn the tortoise 1/3 of 360 degrees to the right --#2
+        Tortoise.turn(120);
+      }
+      catch (RuntimeException re)
+      {
+        MessageBox.showMessage("Hold up: " + re);
+      }
+      //    Repeat --#3.2
     }
-    catch (RuntimeException re)
-    {
-      MessageBox.showMessage("Hold up: " + re);
-    }
-    //    Repeat --#3.2
   }
 }
