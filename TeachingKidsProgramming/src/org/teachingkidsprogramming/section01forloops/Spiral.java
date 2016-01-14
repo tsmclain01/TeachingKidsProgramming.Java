075b7d4ba9e8cfc14050f988aa1f96a84bf2513a
tsmclain01 <tsmclain01@gmail.com>
2016-01-14 11:24:13 -0600

    Added colors to the Color Wheel

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
@@ -1,6 +1,8 @@
 package org.teachingkidsprogramming.section01forloops;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Purples;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
 public class Spiral
@@ -12,14 +14,18 @@ public class Spiral
     //    Make the tortoise go as fast as possible --#4
     Tortoise.setSpeed(10);
     //    Add Blue Violet to the Color Wheel --#7
+    ColorWheel.addColor(Purples.BlueViolet);
     //    Add Violet to the Color Wheel --#8
+    ColorWheel.addColor(Purples.Violet);
     //    Add Purple to the Color Wheel --#9
+    ColorWheel.addColor(Purples.Purple);
     //    Do the following 75 times --#3.1
     for (int i = 0; i < 75; i++)
     {
       try
       {
-        //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#6
+        //         Change the pen color of the line the tortoise draws, the next color on the Color Wheel --#6
+        Tortoise.setPenColor(ColorWheel.getNextColor());
         //         Move the tortoise 5 times the current line number you are drawing --#5
         Tortoise.move(5 * i);
         //         Turn the tortoise 1/3 of 360 degrees to the right --#2
